c290f484026f7bada2c58debcbad05067700929c
robotoART <steellamb.bert@gmail.com>
2018-01-24 20:00:39 -0600

    Add empty map and places list
    feat: Adds a responsive design web page with a placeholder google maps
    api map and an empty list of places navigation bar.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
+var map;
+
 // Model data of all places
+var locations = []
 
-//View
 
-//ViewModel
+// View of Google Map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 39.4, lng: -104.990251},
+          zoom: 9,
+          mapTypeControl: false
+        });
+    ko.applyBindings(new ViewModel());
+}
+
+
+// ViewModel
+var ViewModel = function() {
+    var self = this;
+    var infowindow = new google.maps.InfoWindow({
+      maxWidth: 250
+    });
+    var bounds = new google.maps.LatLngBounds();
+};
+
+var mapError = ko.observable(false);
+var googleMapError = () => {
+  document.getElementById("map").innerHTML = "<h1>There was an unknown problem loading the google map.</h1>";
+};
